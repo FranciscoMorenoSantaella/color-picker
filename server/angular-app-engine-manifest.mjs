4264162635692568a20e1d20ae81e5109fe3ef74
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2024-12-22 23:18:32 +0100

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'https://franciscomorenosantaella.github.io/color-picker',
+  basePath: '/',
   entryPoints: {
     '': () => import('./main.server.mjs')
   },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,12 +2,12 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'https://franciscomorenosantaella.github.io/color-picker',
+  baseHref: '/',
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 6065, hash: 'd5b718a69319e7fbba483e66fdeafa44eea05920b9770163e2d6f9f929c2f8cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1178, hash: 'a11834e0046bea11541c64c815e096a35871c7032bdd0dae3c13b008d539fc61', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6011, hash: 'a9427af70cf24ef827172804f18aa70c6a45912322576740b206171992aefb42', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1124, hash: '900a04b91fa7e66991d12200c3599540e9d226c2eb03663a6cb1fe08cc0df845', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-LITYSAE5.css': {size: 305342, hash: 'UGhU8yTJzEs', text: () => import('./assets-chunks/styles-LITYSAE5_css.mjs').then(m => m.default)}
   },
 };
